816a73093e9901fadb63bf9b739af96bb452df1e
Andres Cagua <af.cagua1011@gmail.com>
2023-03-11 12:20:59 -0500

    remove lazy load for ux experience

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import styles from '../styles/ItemCard.module.css';
 import { currency } from '../utils/formatter';
 import { Link } from 'react-router-dom';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export const ItemCard = ({ id, brand, model, imgUrl, price }) => {
   const handleOnClick = () => {
@@ -13,7 +12,7 @@ export const ItemCard = ({ id, brand, model, imgUrl, price }) => {
 
   return (
     <div className={styles.container}>
-      <LazyLoadImage src={imgUrl} alt={model} />
+      <img src={imgUrl} alt={model} />
       <p className={styles.specs}>
         {model} by <strong>{brand}</strong>
       </p>
